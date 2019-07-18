ef67f5861728101d688e5cac48a00fbccba263d2
Huabuxiu <1347638343@qq.com>
2019-07-18 17:15:45 +0800

    map

--- jsHelloword.js
+++ jsHelloword.js
@@ -1,1 +1,13 @@
-document.write("<h1> h1 标签</h1>")
+
+var testArray = ['1','2','3'];
+
+console.log(testArray);
+
+var ansArray = testArray.map(function(i){
+  var obj = {};
+  obj.label = i.toString();
+  obj.value = i.toString();
+  return obj;
+});
+
+console.log(ansArray);
---
+++ objetc.js
@@ -0,0 +1,12 @@
+/*var f = function () {
+    console.log('1');
+  }
+  
+  function f() {
+    console.log('2');
+  }
+  
+  f() // 1;
+  */
+ var obj = new Object();
+ console.log(1 + obj );
